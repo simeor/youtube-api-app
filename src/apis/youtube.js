e7f76e3c58ac5af8d5e673e6e5f9bf6102ce45f7
simeo russo <simeo94@hotmail.com>
2020-10-14 16:15:17 +0100

    first push

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = {process.env.REACT_APP_SECRET_CODE}; // must be local host - API lock
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params:{
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+
+  state = { term: ""};
+
+  onInputChange = (e) => {
+    this.setState({term: e.target.value})
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render(){
+    return(
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Serach</label>
+            <input value={this.state.term} type="text"
+              onChange={this.onInputChange}/>
+          </div>
+        </form>
+      </div>
+   );
+  }
+}
+
+export default SearchBar;
